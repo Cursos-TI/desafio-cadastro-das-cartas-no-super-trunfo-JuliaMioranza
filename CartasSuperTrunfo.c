f1c5ef4248a1176dbdeaf1901fe0107e409135d7
JuliaMioranza <juuh.m19@gmail.com>
2025-04-12 01:05:09 +0000

    Nível novato

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,77 +1,72 @@
 #include <stdio.h>
 
 int main (){
-// Variáveis sobre a primeira carta.
-    char estado1;
-    char codigo1 [4];
-    char nomeCidade1 [50];
-    int populacao1;
-    float area1;
-    float pib1;
-    int pontosTuristicos1;
+// Nível Novato
+//Declaração de variáveis:
+char Estado1, Estado2, Codigo1 [5], Codigo2 [5], Cidade1 [50], Cidade2 [50];
+int Populacao1, Populacao2, PontosTuristicos1, PontosTuristicos2;
+float Area1, Area2, PIB1, PIB2;
 
-// Variáveis sobre a segunda carta.
-   char estado2;
-   char codigo2 [4];
-   char nomeCidade2 [50];
-   int populacao2;
-   float area2;
-   float pib2;
-   int pontosTuristicos2;
 
-// Entrada de dados da primeira carta.
-printf("Cadastro da primeira carta: \n");
-printf("Informe somente a primeira letra do estado: ");
-scanf("%c",&estado1);
-printf("Informe o código da Carta (Ex: A01): ");
-scanf("%s", codigo1);
-printf("Digite o nome da cidade: ");
-scanf("%s", nomeCidade1);
-printf("Digite a população da cidade: ");
-scanf("%d", &populacao1);
-printf("Informe a àrea da cidade em km²: ");
-scanf("%f", &area1);
-printf("Digite o PIB em bilhões de R$: ");
-scanf("%f", &pib1);
-printf("Quantidade de pontos turísticos da cidade: ");
-scanf("%d", &pontosTuristicos1);
+//Entrada de dados:
+printf("\n Bem-vindo(a), ao super Trunfo!\n");
+printf("\nCarta 1!\n");
+printf("Informe somente uma letra de um Estado: \n");
+scanf(" %c",&Estado1);
+printf("Digite o código da sua carta (Ex: A01): \n");
+scanf("%s",&Codigo1);
+printf("Digite o nome da cidade: \n");
+scanf("%s",&Cidade1);
+printf("Informa a população da cidade (sem espaços): \n");
+scanf("%d",&Populacao1);
+printf("Digite a Área em km²: \n");
+scanf("%f",&Area1);
+printf("Informe o PIB: \n");
+scanf("%f",&PIB1);
+printf("Informe quantos pontos turísticos a cidade possui: \n");
+scanf("%d",&PontosTuristicos1);
 
-// Entrada de dados da segunda carta.
-printf("\nCadastro da segunda carta: \n");
-printf("Informe somente a primeira letra do estado: ");
-scanf(" %c", &estado2);
-printf("Informe o código da carta (Ex: B02): ");
-scanf("%s", codigo2);
-printf("Digite o nome da cidade: ");
-scanf("%s", nomeCidade2);
-printf("Digite a população da cidade: ");
-scanf("%d", &populacao2);
-printf("Informe a área da cidade em km²: ");
-scanf("%f", &area2);
-printf("Digite o PIB em bilhões de R$: ");
-scanf("%f", &pib2);
-printf("Quantidade de pontos turísticos da cidade: ");
-scanf("%d", &pontosTuristicos2);
 
-// Exibição de dados
-printf("\nCarta 1:\n");
-printf("Estado: %c\n", estado1);
-printf("Código: %s\n", codigo1);
-printf("Nome da Cidade: %s\n", nomeCidade1);
-printf("População: %d\n", populacao1);
-printf("Área: %.2f km²\n", area1);
-printf("PIB: %.2f bilhões de reais \n", pib1);
-printf("Número de pontos Turísticos: %d\n", pontosTuristicos1);
+printf("\nCarta 2!\n");
+printf("Informe somente uma letra de um Estado: \n");
+scanf(" %c",&Estado2);
+printf("Digite o código da sua carta (Ex: B02): \n");
+scanf("%s",&Codigo2);
+printf("Digite o nome da cidade (sem espaços): \n");
+scanf("%s",&Cidade2);
+printf("Informa a população da cidade: \n");
+scanf("%d",&Populacao2);
+printf("Digite a Área em km²: \n");
+scanf("%f",&Area2);
+printf("Informe o PIB: \n");
+scanf("%f",&PIB2);
+printf("Informe quantos pontos turísticos a cidade possui: \n");
+scanf("%d",&PontosTuristicos2);
 
-printf("\nCarta 2: \n");
-printf("Estado: %c\n",estado2);
-printf("Código: %s\n", codigo2);
-printf("Nome da cidade: %s\n", nomeCidade2);
-printf("População: %d\n", populacao2);
-printf("Área: %.2f km²\n", area2);
-printf("PIB: %.2f bilhões de reais\n", pib2);
-printf ("Número de Pontos Turísticos: %d\n", pontosTuristicos2);
+// Saída de dados:
+printf("\nCarta 1!\n");
+printf("Estado: %c\n", Estado1);
+printf("Código: %s\n",Codigo1);
+printf("Cidade: %s\n",Cidade1);
+printf("População: %d\n",Populacao1);
+printf("Área: %.2f km²\n",Area1);
+printf("PIB: %.2f Bilhões de reais\n",PIB1);
+printf("Pontos Turísticos: %d\n",PontosTuristicos1);
+
+printf("\nCarta 2!\n");
+printf("Estado: %c\n", Estado2);
+printf("Código: %s\n",Codigo2);
+printf("Cidade: %s\n",Cidade2);
+printf("População: %d\n", Populacao2);
+printf("Área: %.2f km²\n",Area2);
+printf("PIB: %.2f Bilhões de reais\n",PIB2);
+printf("Pontos Turísticos: %d\n",PontosTuristicos2);
+
+printf("\nFim de jogo!\n");
 
 return 0;
-}
 
+
+
+
+}
